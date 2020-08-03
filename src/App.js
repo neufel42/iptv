105da0286229854758940795dbcffeac3a66d1d5
Dave Neufeld <dave.neufeld@gmail.com>
2020-08-02 23:52:48 -0400

    Update loading from local json file

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import NavbarBrand from "react-bootstrap/NavbarBrand";
 import Nav from "react-bootstrap/Nav";
-import axios from 'axios';
+//import axios from 'axios';
 import ReactPlayer from "react-player";
 import './App.css';
 import Splash from "./components/Splash";
+import m3uList from "./sources/m3u"
 
 // these channels are excluded as their CORS policies don't allow them to load
-const excludedChannels = [
-    "tv.28",
-    "tv.37",
-    "tv.58",
-    "tv.21",
-    "tv.36",
-    "tv.49",
-    "tv.63",
-    "tv.56"
-];
+//const excludedChannels = [
+//    "tv.28",
+//    "tv.37",
+//    "tv.58",
+//    "tv.21",
+//    "tv.36",
+//    "tv.49",
+//    "tv.63",
+//    "tv.56"
+//];
 
 class App extends Component {
 
@@ -39,29 +40,25 @@ class App extends Component {
     }
 
     async getChannelList() {
-        const res = await axios.get('https://i.mjh.nz/nz/tv.json');
-        const channelList = res.data;
+        // const res = await axios.get('https://i.mjh.nz/nz/tv.json');
+        // const channelList = res.data;
+        console.log(m3uList); 
+        const channelList = m3uList.channels;
+        console.log(channelList);
 
-        Object.keys(channelList)
-            .filter(key => excludedChannels.includes(key))
-            .forEach(key => delete channelList[key]);
+        //Object.keys(channelList)
+        //    .filter(key => excludedChannels.includes(key))
+        //    .forEach(key => delete channelList[key]);
         
         this.setState({
-            channels: Object.values(channelList).sort((a, b) => {
-                if (a.channel < b.channel) {
-                    return -1;
-                }
-                if (a.channel > b.channel) {
-                    return 1;
-                }
-                return 0;
-            })
+            channels: channelList
         });
     }
 
     getSecureStreamingUrl(channel) {
-        const uri = channel.mjh_master.split('//')[1];
-        return 'https://' + uri;
+        //const uri = channel.mjh_master.split('//')[1];
+        //return 'https://' + uri;
+        return channel.url;
     }
 
     chooseChannel(e, channel) {
@@ -73,7 +70,7 @@ class App extends Component {
 
     render() {
         const channels = this.state.channels.map(ch => (
-            <NavDropdown.Item key={ch.channel || Math.random() * (100 - 50) + 50} onClick={(e) => this.chooseChannel(e, ch)}>{ch.name}</NavDropdown.Item>));
+            <NavDropdown.Item key={ch.id} onClick={(e) => this.chooseChannel(e, ch)}>{ch.name}</NavDropdown.Item>));
 
         return (
             <Container fluid style={{backgroundColor: 'black'}}>
